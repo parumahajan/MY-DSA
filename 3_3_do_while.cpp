7dfed4f0431ef658d910f5edf7fc80d40240f66e
Pranav Mahajan <94169804+parumahajan@users.noreply.github.com>
2025-03-02 08:59:27 +0530

    Add files via upload

---
+++ 3_3_do_while.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+    int main(){
+
+    int i=1;
+    do{
+        cout << "The value of i is: " << i << endl;
+        i++;
+    }
+    while(i<=5);
+    return 0;
+}
+
+/*
+    INIT
+    do{
+        STATEMENT 
+        INC
+    }
+    while(COND);
+*/
