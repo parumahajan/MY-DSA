93b6deb58808f838536ff39fd803c2f7c85997a4
Pranav Mahajan <94169804+parumahajan@users.noreply.github.com>
2025-03-02 08:59:07 +0530

    Add files via upload

---
+++ 3_2_while_loop.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<=5){
+        cout << "The current value of i is: " << i << endl;
+        i++;
+    }
+    cout << "The final value of i is: " << i << endl;
+    return 0;
+}
+
+
+/*  init;
+    while(cond){
+        statement;
+        inc/dec;
+    }
+*/
