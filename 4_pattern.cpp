944718e5b2d247682930cdb02383f0de4bb6a2e8
Pranav Mahajan <94169804+parumahajan@users.noreply.github.com>
2025-03-03 14:13:57 +0530

    Add files via upload

---
+++ 4_pattern.cpp
@@ -0,0 +1,14 @@
+/*
+
+While solving patterns qns, first find the logic behind the pattern 
+before jumping straight to code
+
+Dry run the output first, from there you'll get the logic 
+
+Note: Whenever an Alphabetical Pattern comes, first write it in the form of numbers, 
+and then start dry running it
+
+From there, you'll get the logic, and can easily convert the logic into char 
+to get the desired alphabetical pattern
+
+*/
