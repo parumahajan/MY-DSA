020e59498f3459bbaa196079ca43ec611b493a4e
Pranav Mahajan <94169804+parumahajan@users.noreply.github.com>
2025-03-08 20:00:07 +0530

    Add files via upload

---
+++ p8_1_Switch_Case.cpp
@@ -0,0 +1,19 @@
+//CHARACTER AS INPUT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch = '1';
+    switch(ch) {
+        case '1':
+            cout << "Monday";
+            break;
+        case '2':
+            cout << "Tuesday";
+            break;
+        default:
+            cout << "Invalid input";
+    }
+    return 0;
+}
